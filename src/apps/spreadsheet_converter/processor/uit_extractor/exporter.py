5d16957026a4ef4c84ae3c8a5eeda32a135a8317
elo34 <evsenik23@mail.ru>
2026-04-07 22:14:43 +0300

    fix: fix conflicts

--- src/apps/spreadsheet_converter/processor/uit_extractor/exporter.py
+++ src/apps/spreadsheet_converter/processor/uit_extractor/exporter.py
@@ -7,13 +7,8 @@ def generate_xlsx(data_list, output_path):
     ws = wb.active
     ws.title = 'Data'
 
-<<<<<<< HEAD
-    for code in uit_codes:
-        ws.append([code])
-=======
     for item in data_list:
         ws.append([item])
->>>>>>> 8c19220 (feature: update spreadsheet_converter)
 
     wb.save(output_path)
     wb.close()
